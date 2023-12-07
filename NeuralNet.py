50fdfec1cd2c1f6b80af46e551eaf166971327fc
sierranmorales <143547246+sierranmorales@users.noreply.github.com>
2023-12-06 17:17:54 -1000

    Update NeuralNet.py

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,16 +1,31 @@
-from keras.applications.vgg19 import VGG19
-from keras import models
-from keras import layers
-
+from tensorflow.keras.applications.vgg19 import VGG19
+from tensorflow.keras import models
+from tensorflow.keras import layers
+import tensorflow as tf
+from tensorflow import data as tf_data
+from sklearn.model_selection import train_test_split
+from astropy.io import fits 
+import os
 from PIL import Image
 import numpy as np
 
-#
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+
+# -----------------
 #   Data
-#
+# -----------------
 
 
 #
+#   Preprocessing to VGG19 standard
+#
+def preprocess_patch(patch):
+    # Normalize pixel values (assuming the data range is 0-255)
+    patch = (patch - patch.min())/(patch.max()-patch.min())
+    return patch
 
 def read_supernova_locations(filename):
     supernova_locations = []
@@ -27,53 +42,66 @@ def check_supernova_in_patch(supernova_locations, x_patch, y_patch, patch_size=3
     return False
 
 def process_image_pair(new_image_path, ref_image_path, supernova_locations):
-    new_image = Image.open(new_image_path)
-    ref_image = Image.open(ref_image_path)
+    with fits.open(new_image_path) as new_hdul:
+        new_image_data = new_hdul[1].data
 
-    for i in range(0, new_image.height, 32):
-        for j in range(0, new_image.width, 32):
-            new_patch = np.array(new_image.crop((j, i, j + 32, i + 32)))
-            ref_patch = np.array(ref_image.crop((j, i, j + 32, i + 32)))
+    with fits.open(ref_image_path) as ref_hdul:
+        ref_image_data = ref_hdul[1].data
 
-            combined_patch = np.stack([new_patch, ref_patch, new_patch - ref_patch], axis=0)
+    for i in range(0, new_image_data.shape[0] - 32, 32):
+        for j in range(0, new_image_data.shape[1] - 32, 32):
+            new_patch = new_image_data[i:i + 32, j:j + 32]
+            ref_patch = ref_image_data[i:i + 32, j:j + 32]
 
-            has_supernova = check_supernova_in_patch(supernova_locations, j, i)
+            if new_patch.shape == (32, 32) and ref_patch.shape == (32, 32):
+                sub_patch = preprocess_patch(new_patch - ref_patch)
+                new_patch = preprocess_patch(new_patch)
+                ref_patch = preprocess_patch(ref_patch)
 
-            image_list.append(combined_patch)
-            labels.append(1 if has_supernova else 0)
+                # Stack and then transpose the combined patch
+                combined_patch = np.stack([new_patch, ref_patch, sub_patch], axis=0)
+                combined_patch = np.transpose(combined_patch, (1, 2, 0))  # Reshape from (3, 32, 32) to (32, 32, 3)
 
-# Example usage
-supernova_locations = read_supernova_locations('path_to_your_text_file.txt')
+                has_supernova = check_supernova_in_patch(supernova_locations, j, i)
+
+                image_list.append(combined_patch)
+                labels.append(1 if has_supernova else 0)
+
+
+
 image_list = []
 labels = []
 
-process_image_pair('path_to_new_image.jpg', 'path_to_ref_image.jpg', supernova_locations)
+# Process image pairs and populate image_list and labels
+# Paths for the first set of images and supernova locations
+supernova_locations1 = read_supernova_locations('data/visit_0_3_sn.txt')
+process_image_pair('data/visit_0_3_sn_drz.fits', 'data/visit_0_3_orig_drz.fits', supernova_locations1)
+
+# Paths for the second set of images and supernova locations
+supernova_locations2 = read_supernova_locations('data/visit_1_2_sn.txt')
+process_image_pair('data/visit_1_2_sn_drz.fits', 'data/visit_1_2_orig_drz.fits', supernova_locations2)
 
 # Convert lists to numpy arrays
 image_data = np.array(image_list)
 label_data = np.array(labels)
 
-import tensorflow as tf
+# Shuffle and split the data
+indices = np.arange(image_data.shape[0])
+np.random.shuffle(indices)
+image_data = image_data[indices]
+label_data = label_data[indices]
 
-# Assuming image_list and labels are your processed data
-dataset = tf.data.Dataset.from_tensor_slices((image_list, labels))
+# Split the data into training, validation, and test sets
+# 70% training, 15% validation, 15% test
+train_images, test_images, train_labels, test_labels = train_test_split(
+    image_data, label_data, test_size=0.30, random_state=42)
+val_images, test_images, val_labels, test_labels = train_test_split(
+    test_images, test_labels, test_size=0.50, random_state=42)
 
-# Shuffling the dataset
-dataset = dataset.shuffle(buffer_size=len(image_list))
-
-
-
-#
-
-#Cut out 224x224 pixels or 32x32
-
-# Resizing
-resize_fn = keras.layers.Resizing(224, 224) #VGG19 requirement 224x224 pixels
-
-train_ds = train_ds.map(lambda x, y: (resize_fn(x), y))
-validation_ds = validation_ds.map(lambda x, y: (resize_fn(x), y))
-test_ds = test_ds.map(lambda x, y: (resize_fn(x), y))
-
+# Converting to TensorFlow datasets
+train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+val_ds = tf.data.Dataset.from_tensor_slices((val_images, val_labels))
+test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
 
 # Random data augmentation
 augmentation_layers = [
@@ -88,20 +116,18 @@ def data_augmentation(x):
 
 train_ds = train_ds.map(lambda x, y: (data_augmentation(x), y))
 
-
 # Batch and prefetch
-from tensorflow import data as tf_data
-
 batch_size = 64
 
 train_ds = train_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
-validation_ds = validation_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
+val_ds = val_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()  # Changed from validation_ds to val_ds
 test_ds = test_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
 
 
-#
+
+# -----------------------
 #   Training Model
-#
+# -----------------------
 
 # Create base model
 base_model = VGG19(weights='imagenet', 
@@ -121,38 +147,85 @@ model.add(base_model)
 # Add new layers
 model.add(layers.Flatten())  # Flatten the output of VGG19
 model.add(layers.Dense(256, activation='relu'))  # Add a fully connected layer with 256 neurons
-model.add(layers.Dropout(0.5))  # Dropout layer to reduce overfitting                                      #added
+model.add(layers.Dropout(0.5))  # Dropout layer to reduce overfitting                                   
 model.add(layers.Dense(1, activation='sigmoid'))  # Output layer for binary classification
 
 # Train the model on new data
-model.compile(optimizer=keras.optimizers.Adam(),
-              loss=keras.losses.BinaryCrossentropy(from_logits=False),
-              metrics=[keras.metrics.BinaryAccuracy()])
-epochs = 10
-model.fit(train_ds, 
-          epochs=epochs, 
-          validation_data=validation_ds
-)
+model.compile(optimizer=tf.keras.optimizers.Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+epochs = 100
+history = model.fit(train_ds, epochs=epochs, validation_data=val_ds)
 
-#
+
 # Fine Tuning
 # Adjust layers as required
 for layer in base_model.layers[-4:]:
     layer.trainable = True
 
 model.compile(
-    optimizer=keras.optimizers.Adam(1e-5),  # Low learning rate
+    optimizer=tf.keras.optimizers.Adam(1e-5),  # Low learning rate
     loss='binary_crossentropy',  # Assuming a binary classification (supernova vs no supernova)
-    metrics=[keras.metrics.BinaryAccuracy()]
+    metrics=[tf.keras.metrics.BinaryAccuracy()]
 )
 
-epochs_fine_tuning = 10  # Number of epochs for fine-tuning, adjust as needed
+epochs_fine_tuning = 100  # Number of epochs for fine-tuning, adjust as needed
 
-# Continue training with some layers unfrozen
-model.fit(
+history_fine = model.fit(
     train_ds, 
     epochs=epochs_fine_tuning, 
-    validation_data=validation_ds
+    validation_data=val_ds 
 )
 
-#
+# Save model for future use
+model.save('supernova_identifier.h5')
+
+
+# Plot training history
+def plot_training_history(history, history_fine):
+    # Plot training & validation accuracy values
+    plt.plot(history.history['accuracy'])  # Initial training
+    plt.plot(history.history['val_accuracy'])  # Initial training
+    plt.plot(history_fine.history['binary_accuracy'], linestyle='--')  # Fine-tuning
+    plt.plot(history_fine.history['val_binary_accuracy'], linestyle='--')  # Fine-tuning
+    plt.title('Model Accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train Acc', 'Val Acc', 'Train Fine Acc', 'Val Fine Acc'], loc='upper left')
+    plt.show()
+
+    # Plot training & validation loss values
+    plt.plot(history.history['loss'])  # Initial training
+    plt.plot(history.history['val_loss'])  # Initial training
+    plt.plot(history_fine.history['loss'], linestyle='--')  # Fine-tuning
+    plt.plot(history_fine.history['val_loss'], linestyle='--')  # Fine-tuning
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train Loss', 'Val Loss', 'Train Fine Loss', 'Val Fine Loss'], loc='upper left')
+    plt.show()
+
+
+plot_training_history(history, history_fine)
+
+# Evaluate on test data
+test_loss, test_accuracy = model.evaluate(test_ds)
+print("Test accuracy:", test_accuracy)
+print("Test loss:", test_loss)
+
+# Predict on test data
+y_pred = model.predict(test_ds)
+
+print(y_pred)
+
+y_pred = np.round(y_pred)
+y_true = np.concatenate([y for x, y in test_ds], axis=0)
+
+print(y_pred)
+print(y_true)
+
+# Confusion Matrix
+conf_matrix = confusion_matrix(y_true, y_pred)
+print("Confusion Matrix:\n", conf_matrix)
+
+# Classification Report
+print("Classification Report:\n", classification_report(y_true, y_pred))
+
